049907dbd4d288705f887dffb8b6609b4345591a
Dmitriy Parinov <dimm85g@mail.ru>
2023-01-12 08:44:04 +0300

    Add coding and uncoding methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*  int key = 5;
+        String string = "Наша Таня горько плачет"; */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь к файлу");
+        String filePath = scanner.nextLine();
+        System.out.println("Введите ключ");
+        int key = scanner.nextInt();
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Methods {
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Methods {
+    public static char[] coding (String text, int key) {
+        char[] alphabet = {'à', 'á', 'â', 'ã', 'ä', 'å', '¸', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', '÷', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ', '.', ',', '"', ':', '-', '!', '?', ' '};
+        char[] arrayText = text.toLowerCase().toCharArray();
+        char[] codedText = new char[arrayText.length];
+        int index = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = arrayText[i];
+            int tik = 0;
+            for (int j = 0; j < alphabet.length; j++) {
+                    if (symbol == alphabet[j]) {
+                        index = j;
+                        break;
+                    }
+                    else {
+                        codedText[i] = symbol;
+                        tik++;
+                    }
+                }
+                if (tik < alphabet.length) {
+                    int indexForCode = (index + key) % alphabet.length;
+                    if (indexForCode >= 0) {
+                        codedText[i] = (alphabet[indexForCode]);
+                    } else {
+                        codedText[i] = (alphabet[indexForCode + alphabet.length]);
+                    }
+                }
+        }
+        return codedText;
+    }
+
+    public static char[] uncoding (String text, int key){
+        char[] alphabet = {'à', 'á', 'â', 'ã', 'ä', 'å', '¸', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', '÷', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ', '.', ',', '"', ':', '-', '!', '?', ' '};
+        char[] arrayText = text.toLowerCase().toCharArray();
+        char[] codedText = new char[arrayText.length];
+        int index = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char symbol = arrayText[i];
+            int tik = 0;
+            for (int j = 0; j < alphabet.length; j++) {
+                if (symbol == alphabet[j]) {
+                    index = j;
+                    break;
+                }
+                else {
+                    codedText[i] = symbol;
+                    tik++;
+                }
+            }
+            if (tik < alphabet.length) {
+                int indexForCode = (index + key * -1) % alphabet.length;
+                if (indexForCode >= 0) {
+                    codedText[i] = (alphabet[indexForCode]);
+                } else {
+                    codedText[i] = (alphabet[indexForCode + alphabet.length]);
+                }
+            }
+        }
+        return codedText;
+    }
 }
